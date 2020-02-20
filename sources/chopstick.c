86fbb95224eba327fa060769901f5fbbb1234e9a
Travis Matthews <tmatthew@e1z3r9p9.42.us.org>
2020-02-19 20:58:46 -0800

    unneeded lines

--- sources/chopstick.c
+++ sources/chopstick.c
@@ -6,7 +6,7 @@
 /*   By: ghdesfos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/22 11:55:05 by ghdesfos          #+#    #+#             */
-/*   Updated: 2020/01/25 22:25:01 by ghdesfos         ###   ########.fr       */
+/*   Updated: 2020/02/19 19:56:04 by tmatthew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,18 @@ int		pickup_second_chopstick(t_phil *philo)
 {
 	int s;
 
-	while (1)
+	if (philo->philo_nb % 2 == 0)
 	{
-		if (philo->philo_nb % 2 == 0)
-		{
-			s = pthread_mutex_lock(g_mtx + (philo->philo_nb + 1) % NB_PHILO);
-			(s == 0) ? philo->left_chopstick = 1 : 0;
-		}
-		else
-		{
-			s = pthread_mutex_lock(g_mtx + philo->philo_nb);
-			(s == 0) ? philo->right_chopstick = 1 : 0;
-		}
-		if (s == 0)
-			return (1);
-		usleep(10000);
+		s = pthread_mutex_lock(g_mtx + (philo->philo_nb + 1) % NB_PHILO);
+		(s == 0) ? philo->left_chopstick = 1 : 0;
 	}
+	else
+	{
+		s = pthread_mutex_lock(g_mtx + philo->philo_nb);
+		(s == 0) ? philo->right_chopstick = 1 : 0;
+	}
+	if (s == 0)
+		return (1);
 	return (0);
 }
 
@@ -44,27 +40,24 @@ void	pickup(t_phil *philo)
 {
 	int s;
 
-	while (1)
+	if (philo->philo_nb % 2 == 0)
 	{
-		if (philo->philo_nb % 2 == 0)
-		{
-			s = pthread_mutex_lock(g_mtx + philo->philo_nb);
-			(s == 0) ? philo->right_chopstick = 1 : 0;
-		}
-		else
-		{
-			s = pthread_mutex_lock(g_mtx + (philo->philo_nb + 1) % NB_PHILO);
-			(s == 0) ? philo->left_chopstick = 1 : 0;
-		}
-		if (s == 0)
-		{
-			sleep_global(TIME_THINK);
-			if (pickup_second_chopstick(philo))
-				return ;
-		}
-		else
-			err_exit(s, "pthread_mutex_lock");
+		s = pthread_mutex_lock(g_mtx + philo->philo_nb);
+		(s == 0) ? philo->right_chopstick = 1 : 0;
 	}
+	else
+	{
+		s = pthread_mutex_lock(g_mtx + (philo->philo_nb + 1) % NB_PHILO);
+		(s == 0) ? philo->left_chopstick = 1 : 0;
+	}
+	if (s == 0)
+	{
+		sleep_global(TIME_THINK);
+		if (pickup_second_chopstick(philo))
+			return ;
+	}
+	else
+		err_exit(s, "pthread_mutex_lock");
 }
 
 void	putdown(t_phil *philo)
